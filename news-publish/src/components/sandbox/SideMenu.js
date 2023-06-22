d497c254048231c305214ebf151bc16d874ff3a3
LionX-S <13141096848@163.com>
2023-06-22 20:50:04 +0800

    feat:menu 完成

--- news-publish/src/components/sandbox/SideMenu.js
+++ news-publish/src/components/sandbox/SideMenu.js
@@ -1,13 +1,72 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
 import { Layout, Menu } from "antd";
+import axios from "axios";
 import {
-	UploadOutlined,
+	HomeOutlined,
 	UserOutlined,
-	VideoCameraOutlined
+	SettingOutlined,
+	FormOutlined,
+	AuditOutlined,
+	ToTopOutlined
 } from "@ant-design/icons";
 const { Sider } = Layout;
-export default function SideMenu() {
-  const [collapsed] = useState(false)
+
+// 因为antd采用了新版的写法，所以做下数据改造
+function getItem({ title, key, children }) {
+	const iconList = {
+		"/home": <HomeOutlined />,
+		"/user-manage": <UserOutlined />,
+		"/right-manage": <SettingOutlined />,
+		"/news-manage": <FormOutlined />,
+		"/audit-manage": <AuditOutlined />,
+		"/publish-manage": <ToTopOutlined />
+	};
+	const newChildren =
+		children &&
+		children
+			.filter((item) => item.pagepermisson === 1)
+			.map((item) => getItem(item));
+	return newChildren && newChildren.length === 0
+		? {
+				key,
+				icon: iconList[key],
+				label: title
+		  }
+		: {
+				key,
+				icon: iconList[key],
+				children: newChildren,
+				label: title
+		  };
+}
+
+// submenu keys of first level
+const rootSubmenuKeys = ['/home','/user-manage',"/right-manage","/news-manage","/audit-manage","/publish-manage"];
+function SideMenu(props) {
+	useEffect(() => {
+		axios.get("http://localhost:5000/rights?_embed=children").then((res) => {
+			setMenu(
+				res.data.map((item) => {
+					return getItem(item);
+				})
+			);
+		});
+	}, []);
+	const [collapsed] = useState(false);
+	const [menu, setMenu] = useState([]);
+	const [openKeys, setOpenKeys] = useState([]);
+	const onOpenChange = (keys) => {
+		const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+		if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+			setOpenKeys(keys);
+		} else {
+			setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+		}
+	};
+	const onClickHandle = (eve) => {
+		props.history.push(eve.key);
+	};
 	return (
 		<Sider
 			trigger={null}
@@ -17,25 +76,13 @@ export default function SideMenu() {
 			<Menu
 				theme='dark'
 				mode='inline'
-				defaultSelectedKeys={["1"]}
-				items={[
-					{
-						key: "1",
-						icon: <UserOutlined />,
-						label: "nav 1"
-					},
-					{
-						key: "2",
-						icon: <VideoCameraOutlined />,
-						label: "nav 2"
-					},
-					{
-						key: "3",
-						icon: <UploadOutlined />,
-						label: "nav 3"
-					}
-				]}
+				openKeys={openKeys}
+				onClick={onClickHandle}
+				onOpenChange={onOpenChange}
+				items={menu}
 			/>
 		</Sider>
 	);
 }
+
+export default withRouter(SideMenu);
